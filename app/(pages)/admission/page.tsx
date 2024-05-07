0205a8b882184124a5f5e5d5f6fcb876f8a5bb61
Shah Fahad <shahfaaad530@gmail.com>
2024-05-07 10:32:07 -0700

    updated

--- app/(pages)/admission/page.tsx
+++ app/(pages)/admission/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+import Footer from "@/app/sharecomponents/Footer";
 import Navbar from "@/app/sharecomponents/Navbar";
 import ButtonComponent from "@/app/sharecomponents/animatedButton/Button";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 export default function page() {
+  const [formSelected, setFormSelected] = useState<string | null>(null);
+  const handleFormSelect = (formType: string) => {
+    setFormSelected(formType);
+    // Scroll to the form section with animation
+    const formSection = document.getElementById(formType);
+    // hide the another one
+    const anotherForm = formType === "teacher" ? "parent" : "teacher";
+    const anotherFormSection = document.getElementById(anotherForm);
+    if (anotherFormSection) {
+      anotherFormSection.style.display = "none";
+    }
+    if (formSection) {
+      formSection.style.display = "flex";
+      formSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="text-black overflow-hidden">
       <Navbar />
@@ -24,22 +43,185 @@ export default function page() {
           className="w-[100%] h-full"
         />
         <div className="flex absolute z-20 top-[5%] w-full h-full text-white">
-          <div className="w-1/2 flex flex-col items-center justify-between h-full py-[3%]">
+          <div className="w-1/2 flex flex-col items-center justify-between h-full py-[3%] pb-[5%]">
             <h1 className="text-[40px] ">
               For <span className="font-bold">Teachers</span>
             </h1>
-            <ButtonComponent text="GET  TO KNOW MORE" />
+            <div onClick={() => handleFormSelect("teacher")}>
+              <ButtonComponent text="GET  TO KNOW MORE" />
+            </div>
           </div>
-          <div className="w-1/2 flex flex-col items-center justify-between h-full py-[3%]">
+          <div className="w-1/2 flex flex-col items-center justify-between h-full py-[3%] pb-[5%]">
             <h1 className="text-[40px] ">
               For <span className="font-bold"> Parents</span>
             </h1>
             {/* <button className="uppercase rounded-full px-10 py-2 border-[1px] border-white">
               Get To know more
             </button> */}
-            <ButtonComponent text="GET  TO KNOW MORE" />
+            <div onClick={() => handleFormSelect("parent")}>
+              <ButtonComponent text="GET  TO KNOW MORE" />
+            </div>
           </div>
         </div>
+      </div>
+
+      <div
+        id="teacher"
+        className={`w-full items-center justify-center mt-40 ${
+          formSelected === "teacher" ? "flex" : "hidden"
+        } transition-opacity duration-500 ease-in-out`}
+      >
+        <div className="border border-[#323692] rounded-xl px-[5%] w-[80%] text-[#323692] py-[5%] relative">
+          <Image
+            src={"/birds_form_1.png"}
+            width={500}
+            height={500}
+            alt="birds"
+            className="absolute -top-[120px] right-[20%] h-[200px] w-auto object-contain"
+          />
+          <h1 className="text-[40px] text-center">
+            For <span className="font-bold">Parents</span>
+          </h1>
+          <div className="mt-20 w-full flex flex-col items-center">
+            <div className="flex gap-10 w-full">
+              <div className="w-1/2 flex flex-col gap-5">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#d6d7e9]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#d6d7e9]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#d6d7e9]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#d6d7e9]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#d6d7e9]"
+                />
+              </div>
+              <div className="w-1/2 flex flex-col gap-5">
+                <textarea
+                  rows={7}
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#d6d7e9]"
+                ></textarea>
+                <textarea
+                  rows={3}
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#d6d7e9]"
+                ></textarea>
+              </div>
+            </div>
+            <button className="bg-[#7073b3] w-[50%] mt-10 rounded py-3 text-white">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        id="parent"
+        className={`w-full items-center justify-center mt-40 ${
+          formSelected === "parent" ? "flex" : "hidden"
+        } transition-opacity duration-500 ease-in-out`}
+      >
+        <div className="border border-[#028676] rounded-xl px-[5%] w-[80%] text-[#028676] py-[5%] relative">
+          <Image
+            src={"/birds_form_1.png"}
+            width={500}
+            height={500}
+            alt="birds"
+            className="absolute -top-[120px] right-[20%] h-[200px] w-auto object-contain"
+          />
+          <h1 className="text-[40px] text-center">
+            For <span className="font-bold">Teachers</span>
+          </h1>
+          <div className="mt-20 w-full flex flex-col items-center">
+            <div className="flex gap-10 w-full">
+              <div className="w-1/2 flex flex-col gap-5">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#cce7e4]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#cce7e4]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#cce7e4]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#cce7e4]"
+                />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#cce7e4]"
+                />
+              </div>
+              <div className="w-1/2 flex flex-col gap-5">
+                <textarea
+                  rows={7}
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#cce7e4]"
+                ></textarea>
+                <textarea
+                  rows={3}
+                  placeholder="Name"
+                  className="w-full border outline-[#323692] rounded p-3 bg-[#cce7e4]"
+                ></textarea>
+              </div>
+            </div>
+            <button className="bg-[#4eaba0] w-[50%] mt-10 rounded py-3 text-white">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-[10%] flex items-end">
+        <Image
+          src={"/tree_1.webp"}
+          height={500}
+          width={500}
+          alt="tree"
+          className="w-[80px] h-auto object-contain"
+        />
+        <Image
+          src={"/fox_color_1.webp"}
+          height={500}
+          width={500}
+          alt="tree"
+          className="w-[150px] h-auto object-contain ml-[10%]"
+        />
+        <Image
+          src={"/tree_2.webp"}
+          height={500}
+          width={500}
+          alt="tree"
+          className="w-[50px] h-auto object-contain ml-[70%] mb-[-20px]"
+        />
+      </div>
+      <div className="">
+        <Footer />
       </div>
     </div>
   );
--- app/sharecomponents/Navbar.tsx
+++ app/sharecomponents/Navbar.tsx
@@ -1,18 +1,43 @@
 "use client";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
   const pathname = usePathname();
   console.log("🚀 ~ Navbar ~ pathname:", pathname);
   let color = pathname !== "/" ? "#355496" : "#fff";
+  const [isHeaderShow, setIsHeaderShow] = useState(false);
+
+  let lastScrollTop = 0;
+  useEffect(() => {
+    const handleScroll = () => {
+      const st = window.pageYOffset;
+      //when the user scrolls down 20px from the top of the document, show the button
+      if (st < 10) {
+        setIsHeaderShow(false);
+      } else if (st > lastScrollTop) {
+        setIsHeaderShow(true);
+      } else {
+        setIsHeaderShow(true);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div
-      className={`flex justify-center fixed top-0 left-0 w-full h-[170px] items-center text-[${color}] uppercase z-20`}
+      style={{
+        backgroundColor: isHeaderShow ? "white" : "transparent",
+        transition: "background-color 1s ease",
+        opacity: 1,
+      }}
+      className={`flex justify-center fixed top-0 left-0 w-full h-[140px] items-center text-[${color}] uppercase z-20`}
     >
       <div className="flex w-[80%] items-center justify-between">
-        <div className="w-[30%] flex justify-between">
+        <div className="w-[40%] 2xl:w-[30%] flex justify-between">
           <h1 className="hover:text-[#028676] cursor-pointer">Programs</h1>
 
           <h1 className="hover:text-[#028676] cursor-pointer">
@@ -21,16 +46,20 @@ export default function Navbar() {
 
           <h1 className="hover:text-[#028676] cursor-pointer">Admissions</h1>
         </div>
-        <div className="w-[30%] flex justify-center">
+        <div className="w-[25%] 2xl:w-[30%] flex justify-center">
           <Image
-            src={pathname !== "/" ? "/logo_blue.webp" : "/logo.webp"}
+            src={
+              pathname !== "/" || isHeaderShow
+                ? "/logo_blue.webp"
+                : "/logo.webp"
+            }
             height={200}
             width={200}
             className="h-[80px] w-auto object-contain"
             alt="logo"
           />
         </div>
-        <div className="w-[30%] flex justify-between">
+        <div className="w-[40%] 2xl:w-[30%]  flex justify-between">
           <div className="flex-1">
             <h1 className="hover:text-[#028676] cursor-pointer">
               Clarity By csi
