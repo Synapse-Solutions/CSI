65369982f0ebc0034e816279df25d66bb19c8918
Shah Fahad <shahfaaad530@gmail.com>
2024-06-05 10:55:08 -0700

    updated

--- app/sharecomponents/BirdCard.tsx
+++ app/sharecomponents/BirdCard.tsx
@@ -42,8 +42,17 @@ export default function BirdCard(props: Props) {
         </div>
       </div>
       <div
-        className={`md:hidden cursor-pointer h-[180px] xl:h-[230px] rounded-[12px] w-[calc(80vw)] z-[9999999] xl:w-[400px] object-contain p-5 text-white ${props.bgColor}`}
+        className={`md:hidden mt-[30px] cursor-pointer h-[180px] xl:h-[230px] rounded-[12px] w-[calc(80vw)] z-[9999999] xl:w-[400px] object-contain p-5 text-white relative ${props.bgColor}`}
       >
+        <div className="absolute top-4 right-3">
+          <Image
+            onClick={props.onClickCross}
+            src={"/X.png"}
+            height={20}
+            width={20}
+            alt="x"
+          />
+        </div>
         <h1 className="mt-2 text-[13px] md:text-[17px]">{props.title}</h1>
         <p className="text-[10px] md:text-[14px] mt-3">{props.description}</p>
         <div className="w-full text-end mt-3 flex justify-end">
