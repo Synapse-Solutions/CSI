dccd12402c6dd4035df0f71e31f5b12a4de4a9a5
Shah Fahad <shahfaaad530@gmail.com>
2024-06-06 05:11:32 -0700

    updated

--- app/sharecomponents/homeHeroSection/HeroSection_CS.tsx
+++ app/sharecomponents/homeHeroSection/HeroSection_CS.tsx
@@ -25,38 +25,49 @@ export default function HeroSection_CS() {
       <div className="absolute top-0 left-0 w-full flex h-full items-center justify-center">
         <div className="flex flex-col items-center text-center">
           <h1 className=" text-[25px] xl:text-[45px] font-bold">
-            Coming Soon !
+            This Page Is Being Updated
           </h1>{" "}
           <p></p>
-          <p className="text-[18px] xl:text-[22px] mt-2 text-center">
-            This Page Is Being Updated
-          </p>
-          {/* <p className="text-[18px] xl:text-[22px] mt-10">
+          {/* <p className="text-[18px] xl:text-[22px] mt-2 text-center">
+          Follow us to see the latest Updates
+          </p> */}
+          <p className="text-[18px] xl:text-[22px] mt-10">
             Follow Us To see the latest Updates
           </p>
           <div className="flex items-center gap-4 mt-3">
+            {/* <a href=""></a>
             <Image
               src={"/linkedinlogo.webp"}
               height={32}
               width={32}
               alt="urdu"
               className=" object-contain"
-            />
-            <Image
-              src={"/facebooklogo.webp"}
-              height={32}
-              width={32}
-              alt="urdu"
-              className=" object-contain"
-            />
-            <Image
-              src={"/instagramlogo.webp"}
-              height={32}
-              width={32}
-              alt="urdu"
-              className=" object-contain"
-            />
-          </div> */}
+            /> */}
+            <a
+              href="https://www.facebook.com/centralschoolinternational"
+              target="_blank"
+            >
+              <Image
+                src={"/facebooklogo.webp"}
+                height={32}
+                width={32}
+                alt="urdu"
+                className=" object-contain"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/central.school.international/?fbclid=IwAR2-tAPKEW4ykF3QuvvUBADiuJxEzVvK3CKJkQU-PceD0p3630Z2wTACHxA"
+              target="_blank"
+            >
+              <Image
+                src={"/instagramlogo.webp"}
+                height={32}
+                width={32}
+                alt="urdu"
+                className=" object-contain"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
