700073741b9fa2dd17970413705925921bc79e7e
unknown <faizanofficial120@gmail.com>
2024-06-26 22:19:48 +0500

    ensure uniqueness of resumes names

--- app/service/upload-resume.tsx
+++ app/service/upload-resume.tsx
@@ -1,4 +1,5 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from 'uuid';
 
 const s3 = new S3Client({
   credentials: {
@@ -8,12 +9,14 @@ const s3 = new S3Client({
   region: 'us-east-1', // Replace with your AWS region
 });
 
-export const uploadToS3 = async (file: any, file_name: any) => {
+export const uploadToS3 = async (file: any, originalFileName: any) => {
   console.log("s3", file);
- 
+  
+  const uniqueFileName = `${uuidv4()}-${originalFileName}`;
+
   const params = {
     Bucket: process.env.NEXT_PUBLIC_S3_BUCKET,
-    Key: `public/resumes/${file_name}`, // Set your desired path
+    Key: `public/resumes/${uniqueFileName}`, // Set your desired path
     Body: file,
   };
 
@@ -22,7 +25,7 @@ export const uploadToS3 = async (file: any, file_name: any) => {
     const response = await s3.send(command);
 
     console.log("File is uploaded successfully.");
-    return `https://central-school-international-resumes.s3.amazonaws.com/public/resumes/${file_name}`;
+    return `https://central-school-international-resumes.s3.amazonaws.com/public/resumes/${uniqueFileName}`;
   } catch (error) {
     console.error("Error uploading file:", error);
     throw error;
